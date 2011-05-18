3d82d60377ef7695b715fca71d9bd988d09c607e
Leonard R Budney <len.budney@gmail.com>
2011-05-18 15:31:10 +0000

    Import of BUDNEY/Email-Fingerprint-0.36 from CPAN.
    gitpan-cpan-distribution: Email-Fingerprint
    gitpan-cpan-version:      0.36
    gitpan-cpan-path:         BUDNEY/Email-Fingerprint-0.36.tar.gz
    gitpan-cpan-author:       BUDNEY
    gitpan-cpan-maturity:     released

--- lib/Email/Fingerprint.pm
+++ lib/Email/Fingerprint.pm
@@ -17,11 +17,11 @@ Email::Fingerprint - Calculate a digest for recognizing duplicate emails
 
 =head1 VERSION
 
-Version 0.35
+Version 0.36
 
 =cut
 
-our $VERSION = '0.35';
+our $VERSION = '0.36';
 
 =head1 SYNOPSIS
 
